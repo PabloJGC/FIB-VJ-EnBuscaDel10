22c9b1e2d93f2519406ef6517dc5745ec0c7ad41
PabloJGC <91844499+PabloJGC@users.noreply.github.com>
2022-03-05 20:26:25 +0100

    Add files via upload

---
+++ 2DGame/tilemaps/celeste.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="celeste" tilewidth="8" tileheight="8" tilecount="260" columns="13">
+ <image source="../01-TBE/01-TBE/images/celeste.png" width="105" height="161"/>
+</tileset>
